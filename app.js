4442247b0cde4f05f8f067d6851a6c4676099337
ryvolum <ryvolum@microsoft.com>
2017-05-17 12:57:51 -0700

    created bot and Bing knowledge graph functionality

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const request = require('request');
+const restify = require('restify');
+const builder = require('botbuilder');
+
+const rootQueryString = `https://www.bingapis.com/api/v6/search?responseFilter=knowledge&appId=${process.env.KNOWLEDGE_APP_ID}&screenshotstyle=small&conversation=true`;
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : '',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : ''
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
+
+const getKnowledge = (search, callback) => {
+    const queryString = `${rootQueryString}&q=${search}`
+    request(queryString, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+        } else {
+            const result = JSON.parse(body);
+            if (result) {
+                callback(null, result);
+            }
+        }
+    });
+}
+
+bot.dialog('/', [
+    (session) => {
+        if (session.message.text.toLowerCase() === "hi" || session.message.text.toLowerCase() === "hello") {
+            session.endDialog("Hey! I'm a robotic head. Ask me about people, places or things");
+        } else {
+            getKnowledge(session.message.text, (error, result) => {
+                if (result && result.entities && result.entities.conversation) {
+                    if (result.entities.conversation.spokenText) {
+                        session.endDialog(result.entities.conversation.spokenText);
+                    } else if (result.entities.conversation.displayText) {
+                        session.endDialog(result.entities.conversation.displayText);
+                    }
+                } else if (result) {
+                    session.endDialog("I'm not really sure");
+                } else {
+                    session.endDialog("My brain seems to be broken right now. I'm not getting results from the API");
+                }
+            })
+        }
+    }
+])
+
+
